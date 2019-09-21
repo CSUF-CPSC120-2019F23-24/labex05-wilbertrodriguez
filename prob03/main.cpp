b4c8efa1af0af32c84ebd611b16c5e7ba58727e3
wilbertrodriguez <wilbertrodriguez@csu.fullerton.edu>
2019-09-21 14:19:28 -0700

    Finished random rectangle maker

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,32 @@
 // Random Rectangle
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+//Added header files to obtain some outside files
+
+int main()
+{
+  const int maxh = 40;
+  //sets constant values
+  int length;
+
+  unsigned tooth = time(0);
+  srand(tooth);
+  //sets time to get a random value
+
+  length = rand() % maxh + 1;
+  //generate random number
+
+  std::string rect;
+  rect.assign(length,'#');
+  //creates as many number characters as needed
+
+  std::cout << rect << "\n";
+  std::cout << rect << "\n";
+  std::cout << rect << "\n";
+  //outputs the rectangle
+
+
+  return 0;
+}
